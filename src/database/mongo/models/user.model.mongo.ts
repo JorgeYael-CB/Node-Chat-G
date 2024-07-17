28a4a77e396cbeded1e1e988a1c836c8960c28d0
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-17 14:22:30 -0600

    Model de usuario agregado

---
+++ src/database/mongo/models/user.model.mongo.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  emai: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  img: {
+    type: String,
+    default: 'https://www.pngkey.com/png/full/72-729716_user-avatar-png-graphic-free-download-icon.png'
+  },
+
+  roles: {
+    type: [String],
+    default: ['USER'],
+    enum: ['USER', 'ADMIN', 'SUPER_USER', 'DEVELOPER'],
+  }
+
+
+}, {
+  timestamps: true,
+});
+
+
+export const UserModel = model('User', UserSchema);
+
